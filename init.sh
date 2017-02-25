51affa10a68f00312149e025f151c341fefadef2
amriteya <amriteyapandey@gmail.com>
2017-02-25 20:57:55 +0530

    Removed excess code from init

--- init.sh
+++ init.sh
@@ -5,10 +5,5 @@ script_dir=$(dirname $0)
 if [ $script_dir == '.' ]; then
 	script_dir="$current_dir"
 fi
-RAND_FILE=$(ls $script_dir/bg | sed -n "$((RANDOM%$(ls $script_dir/bg | wc -l)+1))p")
-echo $RAND_FILE
-img_dir="${script_dir}/bg/${RAND_FILE}"
-echo $img_dir
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'$img_dir'"'
 
 (crontab -l ; echo "* * * * * $script_dir/wall.sh") | crontab -
