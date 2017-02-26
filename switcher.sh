ff03e54b16251e099912be51bb5e2632c9e4a2df
amriteya <amriteyapandey@gmail.com>
2017-02-26 10:51:09 +0530

    Removed wget. switched to curl

--- switcher.sh
+++ switcher.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-IMAGE_LINK=$(curl "http://www.nationalgeographic.com/photography/photo-of-the-day/" | grep -w "aemLeadImage" | sed "s/,'aemLeadImage': '//" | sed "s/\/'//")
-wget -A.jpg $IMAGE_LINK
-mv *.tmp bg/image.jpg
-
 source dir-resolver.sh
+
+IMAGE_LINK=$(curl "http://www.nationalgeographic.com/photography/photo-of-the-day/" | grep -w "aemLeadImage" | sed "s/,'aemLeadImage': '//" | sed "s/\/'//")
+curl -X GET $IMAGE_LINK/ > "${DIR}/bg/image.jpg"
 
 IMG_DIR="${DIR}/bg/image.jpg"
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'$IMG_DIR'"'
