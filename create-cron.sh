609ac34c51bbfc82e0fcb4e78a9f7351cd1769dd
amriteya <amriteyapandey@gmail.com>
2017-02-26 12:18:41 +0530

    Removing extra files

--- create-cron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source dir-resolver.sh
-
-(crontab -l ; echo "* * * * * $DIR/switcher.sh >> $DIR/log 2>&1") | crontab -
-
-#Delete cron
-#crontab -l | grep -v '/Users/amrpandey/dev-amriteya/Projects/Personal/Shall-rename/wall.sh >> /Users/amrpandey/dev-amriteya/Projects/Personal/Shall-rename/log 2>&1'  | crontab  -
--- dir-resolver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CURRENT_DIR=$(pwd)
-DIR=$(dirname $0)
-if [ $DIR == '.' ]; then
-	DIR="$CURRENT_DIR"
-fi
